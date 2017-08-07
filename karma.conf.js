ecaae887424526b625258775c16e1a02121acb56
Mark Timmermann <mtimmermann@hotmail.com>
2017-08-06 21:51:53 -0600

    Wiring up karma code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -29,6 +29,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/**/*.js': ['coverage']
     },
 
 
@@ -37,6 +38,11 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['dots', 'coverage'],
 
+
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
 
     // web server port
     port: 9876,
